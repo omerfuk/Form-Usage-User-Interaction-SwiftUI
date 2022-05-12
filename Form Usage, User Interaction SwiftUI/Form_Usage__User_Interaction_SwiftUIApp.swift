76a535849742cd9b5ba1636e1a365ecca425b977
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-12 12:48:30 +0300

    Initial Commit

---
+++ Form Usage, User Interaction SwiftUI/Form_Usage__User_Interaction_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_Usage__User_Interaction_SwiftUIApp.swift
+//  Form Usage, User Interaction SwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Form_Usage__User_Interaction_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
